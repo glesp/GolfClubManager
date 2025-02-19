32b97f5f9f0163fd8e803c13152aafd71d93572d
Conor Gillespie <gillespie.conor@gmail.com>
2025-02-19 19:35:56 +0000

    progress on the binding on booking dropdown!

--- GolfClubManagerAPI/obj/Debug/net8.0/GolfClubManagerAPI.AssemblyInfo.cs
+++ GolfClubManagerAPI/obj/Debug/net8.0/GolfClubManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GolfClubManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ece0ef9d16998225b168743398be4bf60059e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6657df3b42b5e1bdda961c85c884f03d125032a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GolfClubManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GolfClubManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
